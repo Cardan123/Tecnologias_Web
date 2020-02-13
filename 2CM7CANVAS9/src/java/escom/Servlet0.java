23510be26c7a7743e0583df0bcea01c5dc167f25
Cardan123 <carlosdanielvillena@gmail.com>
2020-02-13 08:11:40 -0600

    Actualizacion

--- 2CM7CANVAS9/src/java/escom/Servlet0.java
+++ 2CM7CANVAS9/src/java/escom/Servlet0.java
@@ -22,7 +22,7 @@ public class Servlet0 extends HttpServlet {
             response.setContentType("text/html;charset=UTF-8");
             String numero = request.getParameter("numero");
             HttpSession session =  request.getSession();
-            session.setAttribute("clave",numero);
+            session.setAttribute("clavei",numero);
             int numero_i =Integer.parseInt(numero);
             PrintWriter out = response.getWriter();
             out.println("<!DOCTYPE html>");
@@ -32,6 +32,15 @@ public class Servlet0 extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<form method = 'get' action = 'Servlet1'>");
+            for(int i = 0; i < numero_i; i++){
+            out.println("Punto: "+i+"<br />");
+            out.println("Cordenada de Xi : <input type='text' name='xi_"+i+"' /> <br />");
+            out.println("Cordenada de Yi : <input type='text' name='yi_"+i+"' /> <br />");
+            out.println("Cordenada de Xf : <input type='text' name='xf_"+i+"' /> <br />");
+            out.println("Cordenada de Yf : <input type='text' name='yf_"+i+"' /> <br />");
+            }
+            out.println("<input type='submit' />");
+            out.println("");
             out.println("</form>");
             out.println("</body>");
             out.println("</html>");
